20e74df021c7381c3b8d080786d9035dd813dec2
tiendat1011 <tiendat9tc@gmail.com>
2023-04-07 00:36:44 +0700

    Test

---
+++ TCPChat/ServerForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TCPChat
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvClientList = new System.Windows.Forms.DataGridView();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.rtbMessage = new System.Windows.Forms.RichTextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientList)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvClientList
+            // 
+            this.dgvClientList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvClientList.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvClientList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientList.Location = new System.Drawing.Point(611, 12);
+            this.dgvClientList.Name = "dgvClientList";
+            this.dgvClientList.RowHeadersWidth = 51;
+            this.dgvClientList.RowTemplate.Height = 24;
+            this.dgvClientList.Size = new System.Drawing.Size(167, 260);
+            this.dgvClientList.TabIndex = 1;
+            this.dgvClientList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvClientList_CellContentClick);
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(611, 337);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(167, 22);
+            this.tbIP.TabIndex = 2;
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(611, 381);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(167, 22);
+            this.tbPort.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(680, 318);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(680, 362);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(611, 421);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(166, 44);
+            this.btnCreate.TabIndex = 6;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // rtbMessage
+            // 
+            this.rtbMessage.Location = new System.Drawing.Point(13, 18);
+            this.rtbMessage.Name = "rtbMessage";
+            this.rtbMessage.ReadOnly = true;
+            this.rtbMessage.Size = new System.Drawing.Size(555, 433);
+            this.rtbMessage.TabIndex = 7;
+            this.rtbMessage.Text = "";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(613, 278);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(165, 37);
+            this.btnRemove.TabIndex = 8;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rtbMessage);
+            this.groupBox1.Location = new System.Drawing.Point(4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(574, 460);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Message";
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 477);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbIP);
+            this.Controls.Add(this.dgvClientList);
+            this.Name = "ServerForm";
+            this.Text = "Server";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientList)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dgvClientList;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.RichTextBox rtbMessage;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ TCPChat/ServerForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace TCPChat
+{
+    public partial class ServerForm : Form
+    {
+        public ServerForm()
+        {
+            InitializeComponent();
+        }
+        TcpListener server;
+        Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();
+
+        void StartServer()
+        {
+            server = new TcpListener(IPAddress.Parse(tbIP.Text), Int32.Parse(tbPort.Text));
+            server.Start();
+            rtbMessage.Text += ("Server đang lắng nghe\n");
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    StreamReader reader = new StreamReader(stream);
+                    string name = reader.ReadToEnd();
+                    Thread thread = new Thread(() => UpdateClientList());
+                    clients.Add(name, client);
+                    DateTime now = DateTime.Now;
+                    rtbMessage.Text += ($"({now.ToString()}) {name} đã kết nối\n");
+                    SendAllClient(name + " đã kết nối ");
+                    Thread thread1 = new Thread(() => HandleClient(name) );
+                    thread1.Start();
+                }
+            });
+        }
+        void HandleClient(string name)
+        {
+            DateTime now = DateTime.Now;
+            TcpClient client = clients[name];
+            while (true)
+            {
+                try
+                {
+                    string msg = ReceiveClient(client);
+                    rtbMessage.Text += ($"({now.ToString()}) {name}: {msg}\n");
+                    SendAllClient(name + " : " + msg);
+                }
+                catch
+                {
+                    clients.Remove(name);
+                    SendAllClient(name + " đã ngắt kết nối");
+                }    
+            }
+        }
+        string ReceiveClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytes = stream.Read(buffer, 0, buffer.Length);
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        void SendAllClient(string msg)
+        {
+            DateTime now = DateTime.Now;
+            TcpClient Client;
+            byte[] buffer = Encoding.UTF8.GetBytes($"({now.ToString()}) {msg}");
+            foreach(var key in clients.Keys)
+            {
+                Client = clients[key];
+                if (Client.Connected)
+                {
+                    NetworkStream stream = Client.GetStream();
+                    stream.Write(buffer, 0, buffer.Length);
+                }    
+            }
+
+        }
+        void UpdateClientList()
+        {
+            List<string> names = new List<string>(clients.Keys);
+            dgvClientList.DataSource = names;
+            dgvClientList.Columns[0].HeaderText = "Name";
+        }
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            btnCreate.Enabled = false;
+            StartServer();
+        }
+        string strg = string.Empty;
+        private void dgvClientList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            strg = dgvClientList.CurrentCell.ToString();
+        }
+        void RemoveClient()
+        {
+            TcpClient client;
+            if (clients.TryGetValue(strg, out client))
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = Encoding.UTF8.GetBytes("Bạn đã bi ngắt kết nối khỏi phòng chat");
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Close();
+                client.Close();
+                clients.Remove(strg);
+                SendAllClient(strg + " đã bị ngắt kết nối");
+            }
+            else
+            {
+                MessageBox.Show("Client " + strg + " not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            RemoveClient();
+        }
+    }
+}
